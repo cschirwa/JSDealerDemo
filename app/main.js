e6723c2958d137578212366d006f6f12d22e02fb
calvin <cschirwa@gmail.com>
2020-06-08 06:42:56 +0200

    updated filters, removed unnc view files

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@
 var app = angular.module('app', ['ngRoute']);
     app.constant("baseUrl","http://localhost:8080");
     var dealerData;
+
     app.controller('AppController', function ($scope) {
     });
     app.config(function($routeProvider, $locationProvider) {
@@ -18,27 +19,48 @@ var app = angular.module('app', ['ngRoute']);
         $locationProvider.html5Mode(true);
     });
 
-    app.controller('LoginController',function($scope) {
-    $scope.credentials = {
-        idNumber:"1234567890123",
-        password :"1@Foobar"
-    }
+
+    app.controller('LoginController', function($scope, $http) {
+
+        var loginVar = this;
+        var idNumber = '';
+        var password = '';
+        var loginData = {
+            'idNumber': '1234567890123',
+            'password': '1@Foobar'
+        };
+
+        loginVar.login = function() {
+            $http.post('https://app.bmw.co.za/dws/CandidateTest/servlet/Login', loginData).then(successCallback, errorCallback);
+        };
+
+
+        function successCallback(){
+            console.log("Success successCallback");
+            window.location = 'http://localhost:8080/dealer.html'
+        }
+
+        function errorCallback(){
+            console.log("errorCallback");
+        }
+
+        // function successAPICallback(data){
+        //   JSONData = data;
+        //   console.log("Success successAPICallback : " + JSON.stringify(JSONData));
+        //   window.location = 'http://localhost:8080/dealerLocator.html';
+        //   // successCallback();
+        // }
+        // function errorAPICallback(){
+        //   console.log("errorAPICallback");
+        // }
+        // function loadDealers(){
+        //   $http.get('https://api.bmw.co.za/webcontact/v1/dealer').then(successAPICallback, errorAPICallback);
+        // }
+
     });
 
-    var login = function() {
-        $http.post('https://app.bmw.co.za/dws/CandidateTest/servlet/Login', credentials).then(successCallback, errorCallback);
-    };
 
-function successCallback(){
-
-    console.log("Success successCallback");
-}
-
-function errorCallback(){
-    console.log("errorCallback");
-}
-
-    app.controller('DealerController',function($scope, $http) {
+app.controller('DealerController',function($scope, $http) {
         try {
             $http.get("https://api.bmw.co.za/webcontact/v1/dealer").then(function(response){
                 $scope.dealerData = response.data;
